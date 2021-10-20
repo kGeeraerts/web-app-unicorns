9f9d0cbbc3b9ad7d13618443ce60f0c6764eecf9
StijnDeLeeuw <61213105+StijnDeLeeuw@users.noreply.github.com>
2021-10-21 00:43:22 +0200

    :passport_control: Stronger passwords

--- DogShop/app/Actions/Fortify/ResetUserPassword.php
+++ DogShop/app/Actions/Fortify/ResetUserPassword.php
@@ -4,21 +4,21 @@ namespace App\Actions\Fortify;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
 
-class ResetUserPassword implements ResetsUserPasswords
-{
+class ResetUserPassword implements ResetsUserPasswords {
     use PasswordValidationRules;
 
     /**
      * Validate and reset the user's forgotten password.
      *
-     * @param  mixed  $user
-     * @param  array  $input
+     * @param mixed $user
+     * @param array $input
      * @return void
+     * @throws ValidationException
      */
-    public function reset($user, array $input)
-    {
+    public function reset($user, array $input) {
         Validator::make($input, [
             'password' => $this->passwordRules(),
         ])->validate();
