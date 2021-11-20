83427ac9ea5ad05b9489173b5b7657e2a44ae796
SilkeVervaecke <silke.vervaecke@student.ehb.be>
2021-11-20 16:12:48 +0100

    📄 :cookie: added cookie banner and cookie policy

---
+++ DogShop/resources/lang/vendor/cookie-consent/en/texts.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    // 'message' => 'Your experience on this site will be improved by allowing cookies.',
+    'message' => 'By clicking “Allow cookies”, you agree Dogshop can store cookies on your device and disclose information in accordance with our :cookie_policy.',
+    'agree' => 'Allow cookies 🍪',
+];
---
+++ DogShop/resources/views/cookie.blade.php
@@ -0,0 +1,134 @@
+<x-app-layout>
+    <x-slot name="title">{{__('Cookie Policy') . ' - ' . config('app.name', 'Laravel')}}</x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-2xl text-gray-900 leading-tight dark:text-white">
+            {{ __('Cookie Policy') }}
+        </h2>
+    </x-slot>
+
+    <div class="font-sans text-gray-900 antialiased">
+        <div class="pt-4 bg-gray-100 dark:bg-gray-700">
+            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+                <div>
+                    <a href="http://127.0.0.1:8000/home">
+                        <picture>
+                            <source srcset="/apple-touch-icon-rounded.png" media="(prefers-color-scheme: dark)">
+                            <img src="/safari-pinned-tab.svg" alt="Logo dogShop" class="w-16 h-16">
+                        </picture>
+                    </a>
+                </div>
+
+                <div
+                    class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-400 shadow-md overflow-hidden sm:rounded-lg prose">
+
+                    <h1>Cookie policy</h1>
+                    <p>This cookie policy (“Policy”) describes what cookies are and how and they&#039;re being used by
+                        the <a target="_blank" rel="nofollow"
+                            href="https://dogshop.desideriushogeschool.be">dogshop.desideriushogeschool.be</a> website
+                        (“Website” or
+                        “Service”) and any of its related products and services (collectively, “Services”). This Policy
+                        is a legally
+                        binding agreement between you (“User”, “you” or “your”) and this Website operator (“Operator”,
+                        “we”, “us” or
+                        “our”). If you are entering into this agreement on behalf of a business or other legal entity,
+                        you represent
+                        that you have the authority to bind such entity to this agreement, in which case the terms
+                        “User”, “you” or
+                        “your” shall refer to such entity. If you do not have such authority, or if you do not agree
+                        with the terms of
+                        this agreement, you must not accept this agreement and may not access and use the Website and
+                        Services. You
+                        should read this Policy so you can understand the types of cookies we use, the information we
+                        collect using
+                        cookies and how that information is used. It also describes the choices available to you
+                        regarding accepting or
+                        declining the use of cookies. For further information on how we use, store and keep your
+                        personal data secure,
+                        see our <a target="_blank" rel="nofollow"
+                            href="https://dogshop.desideriushogeschool.be/privacy-policy">privacy
+                            policy</a>.</p>
+                    <h2>What are cookies?</h2>
+                    <p>Cookies are small pieces of data stored in text files that are saved on your computer or other
+                        devices when
+                        websites are loaded in a browser. They are widely used to remember you and your preferences,
+                        either for a single
+                        visit (through a &quot;session cookie&quot;) or for multiple repeat visits (using a
+                        &quot;persistent
+                        cookie&quot;).</p>
+                    <p>Session cookies are temporary cookies that are used during the course of your visit to the
+                        Website, and they
+                        expire when you close the web browser.</p>
+                    <p>Persistent cookies are used to remember your preferences within our Website and remain on your
+                        desktop or mobile
+                        device even after you close your browser or restart your computer. They ensure a consistent and
+                        efficient
+                        experience for you while visiting the Website and Services.</p>
+                    <p>Cookies may be set by the Website (&quot;first-party cookies&quot;), or by third parties, such as
+                        those who serve
+                        content or provide advertising or analytics services on the Website (&quot;third party
+                        cookies&quot;). These
+                        third parties can recognize you when you visit our website and also when you visit certain other
+                        websites. <a target="_blank" href="https://www.websitepolicies.com/blog/cookies">Click here</a>
+                        to learn more about
+                        cookies and how they work.</p>
+                    <h2>What type of cookies do we use?</h2>
+                    <h3>Necessary cookies</h3>
+                    <p>Necessary cookies allow us to offer you the best possible experience when accessing and
+                        navigating through our
+                        Website and using its features. For example, these cookies let us recognize that you have
+                        created an account and
+                        have logged into that account to access the content.</p>
+                    <h3>Functionality cookies</h3>
+                    <p>Functionality cookies let us operate the Website and Services in accordance with the choices you
+                        make. For
+                        example, we will recognize your username and remember how you customized the Website and
+                        Services during future
+                        visits.</p>
+                    <h2>What are your cookie options?</h2>
+                    <p>If you don’t like the idea of cookies or certain types of cookies, you can change your browser’s
+                        settings to
+                        delete cookies that have already been set and to not accept new cookies. Visit <a
+                            target="_blank" href="https://www.internetcookies.com"
+                            rel="noopener">internetcookies.com</a> to learn more about how to do
+                        this.</p>
+                    <p>Please note, however, that if you delete cookies or do not accept them, you might not be able to
+                        use all of the
+                        features the Website and Services offer.</p>
+                    <h2>Changes and amendments</h2>
+                    <p>We reserve the right to modify this Policy or its terms related to the Website and Services at
+                        any time at our
+                        discretion. When we do, we will revise the updated date at the bottom of this page, post a
+                        notification on the
+                        main page of the Website. We may also provide notice to you in other ways at our discretion,
+                        such as through the
+                        contact information you have provided.</p>
+                    <p>An updated version of this Policy will be effective immediately upon the posting of the revised
+                        Policy unless
+                        otherwise specified. Your continued use of the Website and Services after the effective date of
+                        the revised
+                        Policy (or such other act specified at that time) will constitute your consent to those changes.
+                    </p>
+                    <h2>Acceptance of this policy</h2>
+                    <p>You acknowledge that you have read this Policy and agree to all its terms and conditions. By
+                        accessing and using
+                        the Website and Services you agree to be bound by this Policy. If you do not agree to abide by
+                        the terms of this
+                        Policy, you are not authorized to access or use the Website and Services. This cookie policy was
+                        created with
+                        the <a target="_blank" href="https://www.websitepolicies.com/cookie-policy-generator">cookie
+                            policy
+                            generator</a>.</p>
+                    <h2>Contacting us</h2>
+                    <p>If you have any questions, concerns, or complaints regarding this Policy or the use of cookies,
+                        we encourage you
+                        to contact us using the details below:</p>
+                    <p><a target="_blank" rel="nofollow"
+                            href="https://dogshop.desideriushogeschool.be/contact/create">https://dogshop.desideriushogeschool.be/contact/create</a>
+                    </p>
+                    <p>This document was last updated on November 20, 2021</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
