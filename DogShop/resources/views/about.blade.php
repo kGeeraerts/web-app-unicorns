eec7c5eb835fb87022ce0e19eb1b97ea9c4bce3d
Lore Van Langenhove <lore.van.langenhove@student.ehb.be>
2021-11-21 16:26:38 +0100

    about us page

--- DogShop/resources/views/about.blade.php
+++ DogShop/resources/views/about.blade.php
@@ -6,6 +6,11 @@
         </h2>
     </x-slot>
 
-    <p>Under construction</p>
+    
+<p>De Dogshop is een non-profitorganisatie die als doel heeft een einde te maken aan het lijden van zwerfhonden wereldwijd.
+We zetten ons in om de wereldpopulatie zwerfhonden tegen 2030 met 60% te verminderen. Door middel van onderwijs- en adoptieprojecten helpen we de levens van deze dieren te redden.</p>
+<br>
+<p>Wij geloven dat elke hond dezelfde rechten en verantwoordelijkheden heeft als iedereen in de wereld. We bevorderen mededogen en samenwerking en we streven ernaar om de wereld een betere en vriendelijkere plek te maken.
+Met uw hulp kunnen we de levens van reddingshonden in nood helpen redden.</p>
 
 </x-app-layout>
